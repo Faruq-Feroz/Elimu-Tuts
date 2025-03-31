718a1208d372c2f3f1949a72db3253d540830c9d
Faruq-Feroz <164517766+Faruq-Feroz@users.noreply.github.com>
2025-03-31 18:20:34 +0300

    Update Home.jsx

--- frontend/src/pages/landing/Home.jsx
+++ frontend/src/pages/landing/Home.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../../components/common/Navbar';
 import Footer from '../../components/common/Footer';
-
-// Import placeholders individually
 import Hero from '../../components/landing/Hero';
 import AboutUs from '../../components/landing/AboutUs';
 import Learning from '../../components/landing/Features';
@@ -13,24 +11,35 @@ import PopularCourses from '../../components/landing/Courses';
 import ElimuTutsBanner from '../../components/landing/ElimuTutsBanner';
 import FAQ from '../../components/landing/FAQ';
 
-
 const Home = () => {
   return (
     <div className="home-page">
       <Navbar />
       
       <main>
-        <Hero />
-        <AboutUs />
-        <Learning />
-        <FeatureCards />
-        <PopularSubjects/>
-        <PopularCourses />
+        <section id="hero">
+          <Hero />
+        </section>
+        <section id="about">
+          <AboutUs />
+        </section>
+        <section id="features">
+          <Learning />
+          <FeatureCards />
+        </section>
+        <section id="courses">
+          <PopularSubjects />
+          <PopularCourses />
+        </section>
         <ElimuTutsBanner />
-        <FAQ />
+        <section id="faq">
+          <FAQ />
+        </section>
       </main>
       
-      <Footer />
+      <section id="footer">
+        <Footer />
+      </section>
     </div>
   );
 };
